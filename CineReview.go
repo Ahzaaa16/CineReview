a7f0d9893c784b5ad64064ce6186907d1ba3635d
salsadha <salsabnn13@gmail.com>
2026-05-25 10:45:24 +0700

    -salsa, mengerjakan poin a

--- CineReview.go
+++ CineReview.go
@@ -27,15 +27,31 @@ func main() {
 	for {
 
 		fmt.Println("\n===== CINE REVIEW =====")
-		fmt.Println("1. Cari Film")
-		fmt.Println("2. Cari Judul Film")
-		fmt.Println("3. Urutkan Rating Film")
-		fmt.Println("4. Urutkan Tahun Film")
+		fmt.Println("1. Tambah Film")
+		fmt.Println("2. Edit Film")
+		fmt.Println("3. Hapus Film")
+		fmt.Println("4. Cari Film")
+		fmt.Println("5. Cari Judul Film")
+		fmt.Println("6. Urutkan Rating Film")
+		fmt.Println("7. Urutkan Tahun Film")
+		fmt.Println("8. Tampilkan Semua Film")
 		fmt.Println("0. Keluar")
 		fmt.Print("Pilih menu : ")
 		fmt.Scan(&pilihan)
 
 		if pilihan == 1 {
+
+			tambahFilm(&films)
+
+		} else if pilihan == 2 {
+
+			editFilm(&films)
+
+		} else if pilihan == 3 {
+
+			hapusFilm(&films)
+
+		} else if pilihan == 4 {
 
 			var keyword string
 
@@ -44,7 +60,7 @@ func main() {
 
 			cariFilm(films, keyword)
 
-		} else if pilihan == 2 {
+		} else if pilihan == 5 {
 
 			var judul string
 
@@ -55,18 +71,22 @@ func main() {
 
 			cariJudul(films, judul)
 
-		} else if pilihan == 3 {
+		} else if pilihan == 6 {
 
 			urutRating(&films)
 
 			fmt.Println("\nFilm berhasil diurutkan berdasarkan rating")
 			tampilFilm(films)
 
-		} else if pilihan == 4 {
+		} else if pilihan == 7 {
 
 			urutTahun(&films)
 
 			fmt.Println("\nFilm berhasil diurutkan berdasarkan tahun")
+			tampilFilm(films)
+
+		} else if pilihan == 8 {
+
 			tampilFilm(films)
 
 		} else if pilihan == 0 {
@@ -88,10 +108,91 @@ func tampilFilm(films []Film) {
 	for i = 0; i < len(films); i++ {
 
 		fmt.Println("----------------------")
-		fmt.Println("Judul  :", films[i].Judul)
-		fmt.Println("Genre  :", films[i].Genre)
-		fmt.Println("Tahun  :", films[i].Tahun)
-		fmt.Println("Rating :", films[i].Rating)
+		fmt.Println("No      :", i+1)
+		fmt.Println("Judul   :", films[i].Judul)
+		fmt.Println("Genre   :", films[i].Genre)
+		fmt.Println("Tahun   :", films[i].Tahun)
+		fmt.Println("Rating  :", films[i].Rating)
+	}
+}
+
+// tambah data film (@jebb_24)
+func tambahFilm(films *[]Film) {
+
+	var filmBaru Film
+
+	fmt.Print("Masukkan Judul  : ")
+	fmt.Scan(&filmBaru.Judul)
+
+	fmt.Print("Masukkan Genre  : ")
+	fmt.Scan(&filmBaru.Genre)
+
+	fmt.Print("Masukkan Tahun  : ")
+	fmt.Scan(&filmBaru.Tahun)
+
+	fmt.Print("Masukkan Rating : ")
+	fmt.Scan(&filmBaru.Rating)
+
+	*films = append(*films, filmBaru)
+
+	fmt.Println("Film berhasil ditambahkan")
+}
+
+// edit data film (eel)
+func editFilm(films *[]Film) {
+
+	var nomor int
+
+	tampilFilm(*films)
+
+	fmt.Print("Pilih nomor film yang ingin diedit : ")
+	fmt.Scan(&nomor)
+
+	nomor--
+
+	if nomor >= 0 && nomor < len(*films) {
+
+		fmt.Print("Judul Baru  : ")
+		fmt.Scan(&(*films)[nomor].Judul)
+
+		fmt.Print("Genre Baru  : ")
+		fmt.Scan(&(*films)[nomor].Genre)
+
+		fmt.Print("Tahun Baru  : ")
+		fmt.Scan(&(*films)[nomor].Tahun)
+
+		fmt.Print("Rating Baru : ")
+		fmt.Scan(&(*films)[nomor].Rating)
+
+		fmt.Println("Data film berhasil diubah")
+
+	} else {
+
+		fmt.Println("Nomor film tidak valid")
+	}
+}
+
+// hapus data film (@jebb_24)
+func hapusFilm(films *[]Film) {
+
+	var nomor int
+
+	tampilFilm(*films)
+
+	fmt.Print("Pilih nomor film yang ingin dihapus : ")
+	fmt.Scan(&nomor)
+
+	nomor--
+
+	if nomor >= 0 && nomor < len(*films) {
+
+		*films = append((*films)[:nomor], (*films)[nomor+1:]...)
+
+		fmt.Println("Film berhasil dihapus")
+
+	} else {
+
+		fmt.Println("Nomor film tidak valid")
 	}
 }
 
